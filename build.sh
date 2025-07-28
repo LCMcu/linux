5a388ee5570e1c091bebeb2b6a58c6fb16c90533
LCMcu <1771621931@qq.com>
2025-07-28 02:32:54 -0700

    修改CAN dts

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "错误：请提供一个参数"
+    exit 1
+fi
+
+make ARCH=arm64 CROSS_COMPILE=/home/lc/work/tools/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu- $1 
