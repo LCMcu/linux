3a192ff093fda461d28e5d75be2304be8584bbe0
LCMcu <1771621931@qq.com>
2025-08-20 01:58:37 -0700

    恢复pfe0 PHY ID

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ if [ $# -eq 0 ]; then
     echo "错误：请提供一个参数"
     exit 1
 fi
-
-make ARCH=arm64 CROSS_COMPILE=/home/lc/work/tools/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu- $1 
+echo "参数:$@"
+make ARCH=arm64 CROSS_COMPILE=/home/lc/work/tools/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu- $@ 
